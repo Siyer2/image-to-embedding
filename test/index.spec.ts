54669797a584d2243ea0efd2728f5651f0255fe4
Syam Iyer <iyersyam@gmail.com>
2023-04-24 17:37:03 +1000

    feat: update import

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -2,7 +2,8 @@ import { readFileSync } from 'fs';
 import { convertFromBuffer } from '../src';
 
 describe(convertFromBuffer, () => {
-  const testImage = readFileSync('./test_stitch.jpg');
+  const testImage = readFileSync('test/test_stitch.jpg');
+
   it('should return an embedding from the image', async () => {
     const embedding = await convertFromBuffer(testImage);
     console.log(embedding);
